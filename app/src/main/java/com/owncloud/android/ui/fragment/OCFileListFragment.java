95ac33d0e118da542258d632c30011954dae7a62
tobiasKaminsky <tobias@kaminsky.me>
2023-06-26 11:03:49 +0200

    Correctly update file after sharing
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- app/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
+++ app/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
@@ -1358,12 +1358,6 @@ public class OCFileListFragment extends ExtendedListFragment implements
         }
     }
 
-    public void updateOCFile(OCFile file) {
-        List<OCFile> mFiles = mAdapter.getFiles();
-        mFiles.set(mFiles.indexOf(file), file);
-        mAdapter.notifyItemChanged(file);
-    }
-
     private void updateLayout() {
         // decide grid vs list view
         if (isGridViewPreferred(mFile)) {
