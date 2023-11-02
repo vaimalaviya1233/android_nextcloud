ddbe5eca6738762086d81ce929cfa86682e3f443
alperozturk <alper_ozturk@proton.me>
2023-11-02 12:27:18 +0100

    Add Intent extensions
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

---
+++ app/src/main/java/com/nextcloud/utils/extensions/IntentExtensions.kt
@@ -0,0 +1,45 @@
+/*
+ * Nextcloud Android client application
+ *
+ * @author Alper Ozturk
+ * Copyright (C) 2023 Alper Ozturk
+ * Copyright (C) 2023 Nextcloud GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.nextcloud.utils.extensions
+
+import android.content.Intent
+import android.os.Build
+import android.os.Build.VERSION.SDK_INT
+import android.os.Parcelable
+
+fun <T : java.io.Serializable?> Intent.getSerializableArgument(key: String, type: Class<T>): T? {
+    return if (SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        this.getSerializableExtra(key, type)
+    } else {
+        @Suppress("UNCHECKED_CAST", "DEPRECATION")
+        this.getSerializableExtra(key) as T
+    }
+}
+
+fun <T : Parcelable?> Intent.getParcelableArgument(key: String, type: Class<T>): T? {
+    return if (SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        this.getParcelableExtra(key, type)
+    } else {
+        @Suppress( "DEPRECATION")
+        this.getParcelableExtra(key)
+    }
+}
